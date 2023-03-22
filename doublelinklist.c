b29eb33a8d2506ae667e30cf16e8c14005b4fb24
Zaid Akhtar <zaidakhtr7@gmail.com>
2023-03-22 22:00:24 +0530

    Add files via upload

---
+++ doublelinklist.c
@@ -0,0 +1,220 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *prev, *next;
+};
+struct node *head = NULL, *last = NULL;
+void traversing();
+void insertionatst();
+void insertionatpo();
+void insertionatend();
+void deletionatst();
+void deletionatpo();
+void deletionatend();
+void searching();
+void main()
+{
+    int choice;
+    while (1)
+    {
+        printf("\n1.Display list\n");
+        printf("2.Insertion at start\n");
+        printf("3.Insert at any position\n");
+        printf("4.Insert at the end\n");
+        printf("5.Deletion at start\n");
+        printf("6.Deletion at any position\n");
+        printf("7.Deletion at end\n");
+        printf("8.Searching\n");
+        printf("9.Exit\n");
+        printf("\nWhat do you want:");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            traversing();
+            break;
+        case 2:
+            insertionatst();
+            break;
+        case 3:
+            insertionatpo();
+            break;
+        case 4:
+            insertionatend();
+            break;
+        case 5:
+            deletionatst();
+            break;
+        case 6:
+            deletionatpo();
+            break;
+        case 7:
+            deletionatend();
+            break;
+        case 8:
+            searching();
+            break;
+        case 9:
+            exit(0);
+            break;
+        default:
+            printf("you entered wrong choice\n");
+        }
+    }
+}
+void traversing()
+{
+    struct node *temp;
+    if (head == NULL)
+        printf("List is empty\n");
+    else
+    {
+        temp = head;
+        while (temp != NULL)
+        {
+            printf("data:%d\n", temp->data);
+            temp = temp->next;
+        }
+    }
+}
+void insertionatst()
+{
+    int data;
+    struct node *newnode, *temp;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    printf("Enter number you want to insert:");
+    scanf("%d", &newnode->data);
+    newnode->next = NULL;
+    newnode->prev = NULL;
+    if (head == NULL)
+    {
+        head = temp = newnode;
+    }
+    else
+    {
+        head->prev = newnode;
+        newnode->next = head;
+        head = newnode;
+    }
+}
+
+void insertionatpo()
+{
+    struct node *temp, *newnode;
+    int pos, data, i = 1;
+    newnode = malloc(sizeof(struct node));
+    printf("Enter position where you want to insert:");
+    scanf("%d", &pos);
+    printf("Enter data what you want to insert:");
+    scanf("%d", &newnode->data);
+    temp = head;
+    newnode->next = NULL;
+    while (i < pos - 1)
+    {
+        temp = temp->next;
+        i++;
+    }
+    newnode->prev = temp;
+    newnode->next = temp->next;
+    temp->next = newnode;
+    newnode->next->prev = newnode;
+}
+void insertionatend()
+{
+    int data;
+    struct node *temp, *newnode;
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->prev = NULL;
+    temp->next = NULL;
+    printf("Enter number you want to insert in end: ");
+    scanf("%d", &temp->data);
+    temp->next = NULL;
+    newnode = head;
+    if (head == NULL)
+    {
+
+        head = temp;
+    }
+    else
+    {
+        while (newnode->next != NULL)
+            newnode = newnode->next;
+        temp->prev = newnode;
+        newnode->next = temp;
+    }
+}
+void deletionatst()
+{
+    struct node *temp;
+    if (head == NULL)
+        printf("List is empty");
+    else
+    {
+        temp = head;
+        head = head->next;
+        head->prev = NULL;
+        free(temp);
+    }
+}
+void deletionatpo()
+{
+    struct node *temp, *nextnode;
+    int pos, i = 1;
+    printf("Enter position where you want to delete:");
+    scanf("%d", &pos);
+    temp = head;
+    while (i < pos)
+    {
+        temp = temp->next;
+        i++;
+    }
+    temp->prev->next = temp->next;
+    temp->next->prev = temp->prev;
+    free(temp);
+}
+void deletionatend()
+{
+    struct node *temp, *prev;
+    if (head == NULL)
+    {
+        printf("List is empty\n");
+    }
+    else
+    {
+        temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->prev->next = NULL;
+        free(temp);
+    }
+}
+void searching()
+{
+    struct node *temp;
+    int n, i;
+    temp = head;
+    printf("What do you want to search:");
+    scanf("%d", &n);
+    while (temp != NULL)
+    {
+        if (temp->data == n)
+        {
+            printf("Searching successful\n");
+            i = 1;
+            break;
+        }
+        else
+        {
+            i = 0;
+        }
+        temp = temp->next;
+    }
+    if (i == 0)
+    {
+        printf("Searching unsuccessful\n");
+    }
+}
